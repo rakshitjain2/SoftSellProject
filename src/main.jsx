a9024067d41d7e9c9aa14285f97b8b3d98e8a597
Rakshit Jain <rjrakshit6@gmail.com>
2025-05-11 00:12:14 +0530

    Deleted User module wll be updated in next updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router'
 import Layout from './Layout.jsx'
 // import Home from './components/home.jsx'
 import About from './components/About.jsx'
 import Contact from './components/contactUs.jsx'
-import User from './components/user.jsx'
 import SellHere from './components/SellHere.jsx'
 import HomeP from './components/HomeP.jsx'
 
